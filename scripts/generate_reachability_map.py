830a4496523b3688f7c8d9b612de07d0fa5b4512
Marc Bestmann <marc.bestmann@dlr.de>
2025-03-14 15:46:54 +0100

    working version

--- scripts/generate_reachability_map.py
+++ scripts/generate_reachability_map.py
@@ -16,9 +16,9 @@ from rclpy.node import Node
 
 # TODO make arguments
 # map size si given in x,y,z as tuples of lower and upper limit
-MAP_SIZE = [[-1, 2], [-1, 2], [-1, 1]]  # m
+MAP_SIZE = [[-2, 2], [-2, 2], [-2, 2]]  # m
 VOXEL_SIZE = 0.01  # m
-ANG_STEP_SIZE = math.radians(10)  # rad
+ANG_STEP_SIZE = math.radians(5)  # rad
 
 voxels_in_x = math.ceil((MAP_SIZE[0][1] - MAP_SIZE[0][0]) / VOXEL_SIZE)
 voxels_in_y = math.ceil((MAP_SIZE[1][1] - MAP_SIZE[1][0]) / VOXEL_SIZE)
@@ -63,6 +63,7 @@ class ReachabilityMap:
         self.robot_state = RobotState(self.robot_model)
         self.joint_group_name = joint_group_name
         self.joint_group = self.robot_model.get_joint_model_group(self.joint_group_name)
+        self.min_positions = []
         self.current_positions = []
         self.steps_per_joint = []
         # set all joints to their minimal position at the start
@@ -71,8 +72,6 @@ class ReachabilityMap:
             print(self.joint_group.active_joint_model_names[i])
             print(f"min {self.joint_group.active_joint_model_bounds[i][0].min_position}")
             print(f"max {self.joint_group.active_joint_model_bounds[i][0].max_position}")
-            self.current_positions.append(
-                self.joint_group.active_joint_model_bounds[i][0].min_position)
             min_position = self.joint_group.active_joint_model_bounds[i][0].min_position
             max_position = self.joint_group.active_joint_model_bounds[i][0].max_position
             # we ignore possible positions that are larger than one overall turn
@@ -82,15 +81,18 @@ class ReachabilityMap:
             if max_position >= math.tau/2:
                 min_position = max(
                     self.joint_group.active_joint_model_bounds[i][0].min_position, -math.tau/2)
+            self.current_positions.append(
+                self.joint_group.active_joint_model_bounds[i][0].min_position)
             self.steps_per_joint.append(round((max_position - min_position) / ANG_STEP_SIZE))
+            self.min_positions.append(min_position)
             print(f"limited min {min_position}")
             print(f"limited max {max_position}")
             print("")
         pprint(self.current_positions)
         pprint(self.steps_per_joint)
 
-        # self.endeffector_name = joint_group.eef_name
-        self.endeffector_name = "end_effector"
+        self.endeffector_name = self.joint_group.link_model_names[-1]
+        # self.endeffector_name = "end_effector"
         self.pose_counter = 0
         self.poses_to_compute = 1
         for steps in self.steps_per_joint:
@@ -108,22 +110,23 @@ class ReachabilityMap:
                 self.robot_state.update()
                 # verify if joints are in bounds
                 # todo this is probably not working like this, number of joints does not match
-                # pose = self.robot_state.get_pose(self.endeffector_name)
-                pose = self.robot_state.get_frame_transform(self.endeffector_name)
+                pose = self.robot_state.get_pose(self.endeffector_name)
+                # pose = self.robot_state.get_frame_transform(self.endeffector_name)
                 # print(pose)
-                # print(pose)
-                i_x, i_y, i_z = rmap_entry_from_xyz(pose[0][3], pose[1][3], pose[2][3])
-                # i_x, i_y, i_z = rmap_entry_from_xyz(pose.position.x, pose.position.y, pose.position.z)
+                # i_x, i_y, i_z = rmap_entry_from_xyz(pose[0][3], pose[1][3], pose[2][3])
+                i_x, i_y, i_z = rmap_entry_from_xyz(
+                    pose.position.x, pose.position.y, pose.position.z)
                 self.rmap[i_x, i_y, i_z] = self.rmap[i_x, i_y, i_z] + 1
                 # print(str(self.current_positions))
                 self.pose_counter += 1
                 if self.pose_counter % 1000 == 0:
-                    print(f"Computed {self.pose_counter}/{self.poses_to_compute}")
+                    print(f"{100*self.pose_counter/self.poses_to_compute:.2f}%\r", end="")
                 return
             for j in range(self.steps_per_joint[i]):
-                self.current_positions[i] = j * ANG_STEP_SIZE
+                self.current_positions[i] = self.min_positions[i] + j * ANG_STEP_SIZE
                 try_configurations_recursivly(joint_names[1:], i+1)
 
+        print("")
         try_configurations_recursivly(self.joint_group.active_joint_model_names, 0)
         print("finished")
 
@@ -146,10 +149,11 @@ class ReachabilityMap:
                         marker.ns = "reachability_map"
                         marker.id = id
                         id += 1
-                        marker.type = marker.CUBE
-                        marker.scale.x = VOXEL_SIZE
-                        marker.scale.y = VOXEL_SIZE
-                        marker.scale.z = VOXEL_SIZE
+                        marker.type = marker.SPHERE
+                        MARKER_SCALE = 1.0  # todo make parameter
+                        marker.scale.x = VOXEL_SIZE * MARKER_SCALE
+                        marker.scale.y = VOXEL_SIZE * MARKER_SCALE
+                        marker.scale.z = VOXEL_SIZE * MARKER_SCALE
                         x, y, z = xyz_from_rmap_entry(i, j, k)
                         marker.pose.position.x = x
                         marker.pose.position.y = y
@@ -161,7 +165,7 @@ class ReachabilityMap:
                         marker.color.r = r
                         marker.color.g = g
                         marker.color.b = 0.0
-                        marker.color.a = 0.5  # todo make parameter
+                        marker.color.a = 1.0  # todo make parameter
                         marker_array.markers.append(marker)
 
         self.marker_publisher.publish(marker_array)
@@ -173,8 +177,8 @@ class ReachabilityMap:
 def main():
     parser = argparse.ArgumentParser(description="Generate Reachability Map")
     parser.add_argument("--robot-name", type=str, help="Name of the robot", default="elise")
-    parser.add_argument("--joint-group-name", type=str,
-                        help="Name of the joint group", default="endo")
+    parser.add_argument("--joint-group-name", "-g", type=str,
+                        help="Name of the joint group", default="all")
     args = parser.parse_known_args()[0]
     rmap = ReachabilityMap(args.robot_name, args.joint_group_name)
     rmap.generate_reachability_map()
