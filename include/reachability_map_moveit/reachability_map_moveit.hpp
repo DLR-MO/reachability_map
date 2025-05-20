081e52b720a48a978668ed964dec7157592bf174
Marc Bestmann <marc.bestmann@dlr.de>
2025-05-20 10:22:47 +0200

    refactor formatting

--- include/reachability_map_moveit/reachability_map_moveit.hpp
+++ include/reachability_map_moveit/reachability_map_moveit.hpp
@@ -23,17 +23,20 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 
-namespace reachability_map_moveit {
+namespace reachability_map_moveit
+{
 
-//template <typename C> struct is_vector : std::false_type {};    
-//template <typename T,typename A> struct is_vector< std::vector<T,A> > : std::true_type {};    
+//template <typename C> struct is_vector : std::false_type {};
+//template <typename T,typename A> struct is_vector< std::vector<T,A> > : std::true_type {};
 //template <typename C> inline constexpr bool is_vector_v = is_vector<C>::value;
 
 
 class ReachabilityMapMoveit {
 
- public:
-  ReachabilityMapMoveit(const std::string& joint_group_name, const double voxel_size, const double ang_step_size);
+public:
+  ReachabilityMapMoveit(
+    const std::string & joint_group_name, const double voxel_size,
+    const double ang_step_size);
   void generate_reachability_map();
   void send_marker_message(bool use_sphere, float scale);
   void spin();
@@ -41,21 +44,21 @@ class ReachabilityMapMoveit {
   void load_reachability_map(std::string path);
   void export_pcd(std::string path);
 
- private:
+private:
   void try_configurations_recursively(long unsigned int i);
   uint32_t get_max_value();
 
   rclcpp::Node::SharedPtr node_;
   const std::string joint_group_name_;
-  const std::vector<std::vector<double>> map_size_; //m  
+  const std::vector<std::vector<double>> map_size_; //m
   const double voxel_size_; //m
   const double ang_step_size_; //rad
   rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr marker_pub_;
-  robot_model_loader::RobotModelLoader robot_model_loader_;  
-  const moveit::core::RobotModelPtr& robot_model_;
+  robot_model_loader::RobotModelLoader robot_model_loader_;
+  const moveit::core::RobotModelPtr & robot_model_;
   Bonxai::VoxelGrid<uint32_t> grid_;
   moveit::core::RobotStatePtr robot_state_;
-  const moveit::core::JointModelGroup* joint_group_;
+  const moveit::core::JointModelGroup * joint_group_;
   Bonxai::VoxelGrid<uint32_t>::Accessor accessor_;
   std::vector<std::string> joint_names_;
   std::string tip_name_;
--- src/reachability_map_moveit.cpp
+++ src/reachability_map_moveit.cpp
@@ -5,7 +5,8 @@
 using namespace std;
 
 
-namespace reachability_map_moveit {
+namespace reachability_map_moveit
+{
 
 
 /**
@@ -13,116 +14,130 @@ namespace reachability_map_moveit {
  *
  * @param options node options
  */
-ReachabilityMapMoveit::ReachabilityMapMoveit(const std::string& joint_group_name, const double voxel_size, const double ang_step_size) 
-: node_(rclcpp::Node::make_shared("reachability_map_moveit")), 
-joint_group_name_(joint_group_name), 
-voxel_size_(voxel_size), 
-ang_step_size_(ang_step_size), 
-robot_model_loader_(node_),
-robot_model_(robot_model_loader_.getModel()),
-grid_(voxel_size),
-accessor_(grid_.createAccessor())
+ReachabilityMapMoveit::ReachabilityMapMoveit(
+  const std::string & joint_group_name,
+  const double voxel_size, const double ang_step_size)
+: node_(rclcpp::Node::make_shared("reachability_map_moveit")),
+  joint_group_name_(joint_group_name),
+  voxel_size_(voxel_size),
+  ang_step_size_(ang_step_size),
+  robot_model_loader_(node_),
+  robot_model_(robot_model_loader_.getModel()),
+  grid_(voxel_size),
+  accessor_(grid_.createAccessor())
 {
   // create a Moveit Robot State to compute the forward kinematics
   robot_state_ = std::make_shared<moveit::core::RobotState>(moveit::core::RobotState(robot_model_));
   robot_state_->setToDefaultValues();
   robot_state_->update();
   joint_group_ = robot_model_->getJointModelGroup(joint_group_name_);
-  joint_names_ = joint_group_->getActiveJointModelNames();  
+  joint_names_ = joint_group_->getActiveJointModelNames();
   tip_name_ = joint_group_->getLinkModelNames().back();
 
   // get bounds without more than one turn per joint
   for (long unsigned int i = 0; i < size(joint_names_); ++i) {
-    const moveit::core::JointModel* joint_model = joint_group_->getJointModel(joint_names_[i]);  
-    const moveit::core::JointModel::Bounds& joint_bounds = joint_model->getVariableBounds();
-    const moveit::core::VariableBounds bounds = joint_bounds[0]; //TODO this only works for single DOF joints, either trow an error if its more DoF or use a more complex solution
+    const moveit::core::JointModel * joint_model = joint_group_->getJointModel(joint_names_[i]);
+    const moveit::core::JointModel::Bounds & joint_bounds = joint_model->getVariableBounds();
+    //TODO this only works for single DOF joints, either trow an error if its more DoF or use a more complex solution
+    const moveit::core::VariableBounds bounds = joint_bounds[0];
     double min_pos = bounds.min_position_;
     double max_pos = bounds.max_position_;
     if (min_pos <= -M_PI) {
-        max_pos = std::min(max_pos, M_PI);
+      max_pos = std::min(max_pos, M_PI);
     }
     if (max_pos > M_PI) {
-        min_pos = std::max(min_pos, -M_PI);
+      min_pos = std::max(min_pos, -M_PI);
     }
     current_positions_.push_back(min_pos);
     steps_per_joint_.push_back(round((max_pos - min_pos) / ang_step_size_));
     min_positions_.push_back(min_pos);
-    
-    RCLCPP_INFO(node_->get_logger(), "Joint %s min pos %f, max pos %f configurations.\n", joint_names_[0].c_str(), min_pos, max_pos);
+
+    RCLCPP_INFO(node_->get_logger(), "Joint %s min pos %f, max pos %f configurations.\n",
+        joint_names_[0].c_str(), min_pos, max_pos);
   }
 
   // Compute the total number of configurations to evaluate
   poses_to_compute_ = 1;
-  for (const auto& steps : steps_per_joint_) {
-      poses_to_compute_ *= steps;
+  for (const auto & steps : steps_per_joint_) {
+    poses_to_compute_ *= steps;
   }
   RCLCPP_INFO(node_->get_logger(), "Will need to compute %lu configurations.", poses_to_compute_);
 
   // get the planning scene for collision checking
-  planning_scene_monitor_ = std::make_shared<planning_scene_monitor::PlanningSceneMonitor>(node_, "robot_description");
+  planning_scene_monitor_ = std::make_shared<planning_scene_monitor::PlanningSceneMonitor>(node_,
+      "robot_description");
   planning_scene_ = planning_scene_monitor_->getPlanningScene();
   if (!planning_scene_) {
     RCLCPP_ERROR_ONCE(node_->get_logger(), "failed to connect to planning scene");
   }
 
   // publisher for publishing outgoing messages
-  rclcpp::QoS qos(10);  
+  rclcpp::QoS qos(10);
   qos.transient_local();
-  marker_pub_ = node_->create_publisher<visualization_msgs::msg::MarkerArray>("/reachability_map", qos);
+  marker_pub_ = node_->create_publisher<visualization_msgs::msg::MarkerArray>("/reachability_map",
+      qos);
 }
 
-void ReachabilityMapMoveit::spin(){
-    rclcpp::spin(node_); //todo this whole method is hacky
+void ReachabilityMapMoveit::spin()
+{
+  rclcpp::spin(node_);   //todo this whole method is hacky
 }
 
-void ReachabilityMapMoveit::generate_reachability_map() {
+void ReachabilityMapMoveit::generate_reachability_map()
+{
   try_configurations_recursively(0);
   RCLCPP_INFO(node_->get_logger(), "\nFinished generating the reachability map.");
 }
 
-void ReachabilityMapMoveit::try_configurations_recursively(long unsigned int i) {
+void ReachabilityMapMoveit::try_configurations_recursively(long unsigned int i)
+{
   if (i >= current_positions_.size()) {
-      robot_state_->setJointGroupActivePositions(joint_group_, current_positions_);
-      robot_state_->update();
-      collision_detection::CollisionRequest req;
-      collision_detection::CollisionResult res;
-      planning_scene_->checkCollision(req, res, *robot_state_, planning_scene_->getAllowedCollisionMatrix());
+    robot_state_->setJointGroupActivePositions(joint_group_, current_positions_);
+    robot_state_->update();
+    collision_detection::CollisionRequest req;
+    collision_detection::CollisionResult res;
+    planning_scene_->checkCollision(req, res, *robot_state_,
+        planning_scene_->getAllowedCollisionMatrix());
 
-      if(res.collision) {
-        return;
-      }
-
-      Eigen::Vector3d position = robot_state_->getGlobalLinkTransform(tip_name_).translation();
-      // this automatically initilized voxels with 0 if the voxel is not initialized
-      auto* voxel = accessor_.value(grid_.posToCoord(position.x(), position.y(), position.z()), true);
-      (*voxel)++;
-
-      if (++pose_counter_ % 1000 == 0) {
-          std::cout << "\r" << pose_counter_ * 100.0 / poses_to_compute_ << "% done" << std::flush;
-      }
+    if(res.collision) {
       return;
+    }
+
+    Eigen::Vector3d position = robot_state_->getGlobalLinkTransform(tip_name_).translation();
+      // this automatically initilized voxels with 0 if the voxel is not initialized
+    auto * voxel = accessor_.value(grid_.posToCoord(position.x(), position.y(), position.z()),
+        true);
+    (*voxel)++;
+
+    if (++pose_counter_ % 1000 == 0) {
+      std::cout << "\r" << pose_counter_ * 100.0 / poses_to_compute_ << "% done" << std::flush;
+    }
+    return;
   }
   for (int j = 0; j < steps_per_joint_[i]; ++j) {
-      current_positions_[i] = min_positions_[i] + j * ang_step_size_;
-      try_configurations_recursively(i + 1);
+    current_positions_[i] = min_positions_[i] + j * ang_step_size_;
+    try_configurations_recursively(i + 1);
   }
 }
 
-uint32_t ReachabilityMapMoveit::get_max_value(){
+uint32_t ReachabilityMapMoveit::get_max_value()
+{
   uint32_t max_val = 0;
-  auto maxVisitor = [this, &max_val](const uint32_t& value, const Bonxai::CoordT& coord) {
-    max_val = std::max(max_val, value);
-  };
+  auto maxVisitor = [this, &max_val](const uint32_t & value, const Bonxai::CoordT & coord) {
+      max_val = std::max(max_val, value);
+    };
   grid_.forEachCell(maxVisitor);
   return max_val;
 }
 
 
-void ReachabilityMapMoveit::send_marker_message(bool use_sphere, float scale) {
-  // first remove old marker array message, this is necessary since the number of markers 
+void ReachabilityMapMoveit::send_marker_message(bool use_sphere, float scale)
+{
+  // first remove old marker array message, this is necessary since the number of markers
   // might vary depending on voxel size, therefore, reusing the id does not work
   std::string marker_ns = "reachability_map";
-  visualization_msgs::msg::MarkerArray delete_marker_array_msg = visualization_msgs::msg::MarkerArray();
+  visualization_msgs::msg::MarkerArray delete_marker_array_msg =
+    visualization_msgs::msg::MarkerArray();
   visualization_msgs::msg::Marker delete_maker = visualization_msgs::msg::Marker();
   delete_maker.id = 0;
   delete_maker.ns = marker_ns;
@@ -130,51 +145,54 @@ void ReachabilityMapMoveit::send_marker_message(bool use_sphere, float scale) {
   delete_marker_array_msg.markers.push_back(delete_maker);
   marker_pub_->publish(delete_marker_array_msg);
 
-  visualization_msgs::msg::MarkerArray marker_array;  
+  visualization_msgs::msg::MarkerArray marker_array;
   auto type = visualization_msgs::msg::Marker::CUBE;
-  if (use_sphere){
+  if (use_sphere) {
     type = visualization_msgs::msg::Marker::SPHERE;
   }
   uint32_t id = 0;
   uint32_t max_val = get_max_value();
 
-  auto markerVisitor = [this, &marker_array, &max_val, &id, &type, &scale, &marker_ns](const uint32_t& value, const Bonxai::CoordT& coord) {    
-        double r = (max_val - value) / static_cast<double>(max_val);
-        double g = value / static_cast<double>(max_val);
-        visualization_msgs::msg::Marker marker;
-        marker.header.frame_id = "link1"; //todo hack
-        marker.ns = marker_ns;
-        marker.id = id++;
-        marker.type = type;
-        marker.frame_locked = true;
-        marker.scale.x = voxel_size_ * scale;
-        marker.scale.y = voxel_size_ * scale;
-        marker.scale.z = voxel_size_ * scale;
-        auto pos = grid_.coordToPos(coord);
-        marker.pose.position.x = pos.x;
-        marker.pose.position.y = pos.y;
-        marker.pose.position.z = pos.z;
-        marker.pose.orientation.w = 1.0;
-        marker.color.r = r;
-        marker.color.g = g;
-        marker.color.b = 0.0;
-        marker.color.a = 1.0;
-        marker_array.markers.push_back(marker);
-  };
+  auto markerVisitor = [this, &marker_array, &max_val, &id, &type, &scale,
+      &marker_ns](const uint32_t & value, const Bonxai::CoordT & coord) {
+      double r = (max_val - value) / static_cast<double>(max_val);
+      double g = value / static_cast<double>(max_val);
+      visualization_msgs::msg::Marker marker;
+      marker.header.frame_id = "link1";   //todo hack
+      marker.ns = marker_ns;
+      marker.id = id++;
+      marker.type = type;
+      marker.frame_locked = true;
+      marker.scale.x = voxel_size_ * scale;
+      marker.scale.y = voxel_size_ * scale;
+      marker.scale.z = voxel_size_ * scale;
+      auto pos = grid_.coordToPos(coord);
+      marker.pose.position.x = pos.x;
+      marker.pose.position.y = pos.y;
+      marker.pose.position.z = pos.z;
+      marker.pose.orientation.w = 1.0;
+      marker.color.r = r;
+      marker.color.g = g;
+      marker.color.b = 0.0;
+      marker.color.a = 1.0;
+      marker_array.markers.push_back(marker);
+    };
   grid_.forEachCell(markerVisitor);
   marker_pub_->publish(marker_array);
   RCLCPP_INFO(node_->get_logger(), "Published reachability map markers.");
 }
 
-void ReachabilityMapMoveit::save_reachability_map(std::string path){
+void ReachabilityMapMoveit::save_reachability_map(std::string path)
+{
   ofstream file(path);
   Bonxai::Serialize(file, grid_);
   file.close();
-  
+
   RCLCPP_INFO(node_->get_logger(), "Saved reachability map to file.");
 }
 
-void ReachabilityMapMoveit::load_reachability_map(std::string path){
+void ReachabilityMapMoveit::load_reachability_map(std::string path)
+{
   ifstream file(path);
   char header[256];
   file.getline(header, 256);
@@ -182,28 +200,30 @@ void ReachabilityMapMoveit::load_reachability_map(std::string path){
   grid_ = Bonxai::Deserialize<uint32_t>(file, info);
   RCLCPP_INFO(node_->get_logger(), "Loaded reachability map from file.");
 }
-void ReachabilityMapMoveit::export_pcd(std::string path){
+void ReachabilityMapMoveit::export_pcd(std::string path)
+{
   pcl::PointCloud<pcl::PointXYZRGBL> cloud;
   // we need to publish it als unorganized cloud
-  cloud.width    = grid_.activeCellsCount();
-  cloud.height   = 1;
+  cloud.width = grid_.activeCellsCount();
+  cloud.height = 1;
   cloud.is_dense = true;
   cloud.resize (cloud.width * cloud.height);
   uint32_t i = 0;
   uint32_t max_val = get_max_value();
 
-  auto cloudVisitor = [this, &cloud, &i, &max_val](const uint32_t& value, const Bonxai::CoordT& coord) {        
-        auto pos = grid_.coordToPos(coord);
-        cloud.points[i].x = pos.x;
-        cloud.points[i].y = pos.y;
-        cloud.points[i].z = pos.z;        
-        cloud.points[i].r = ((max_val - value) / static_cast<double>(max_val))*255;
-        cloud.points[i].g = (value / static_cast<double>(max_val))*255; 
-        cloud.points[i].b = std::min((uint32_t)value, (uint32_t)255);
-        cloud.points[i].a = 1.0;
-        cloud.points[i].label = value;
-        i++;
-  };
+  auto cloudVisitor = [this, &cloud, &i, &max_val](const uint32_t & value,
+    const Bonxai::CoordT & coord) {
+      auto pos = grid_.coordToPos(coord);
+      cloud.points[i].x = pos.x;
+      cloud.points[i].y = pos.y;
+      cloud.points[i].z = pos.z;
+      cloud.points[i].r = ((max_val - value) / static_cast<double>(max_val)) * 255;
+      cloud.points[i].g = (value / static_cast<double>(max_val)) * 255;
+      cloud.points[i].b = std::min((uint32_t)value, (uint32_t)255);
+      cloud.points[i].a = 1.0;
+      cloud.points[i].label = value;
+      i++;
+    };
   grid_.forEachCell(cloudVisitor);
 
   pcl::io::savePCDFileASCII (path, cloud);
@@ -213,25 +233,30 @@ void ReachabilityMapMoveit::export_pcd(std::string path){
 
 //todo add export to pointcloud or csv method
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
   rclcpp::init(argc, argv);
 
   argparse::ArgumentParser args("Reachability Map Moveit");
   args.add_argument("joint-group-name").help("Name of the joint group that you want to use.");
   args.add_argument("voxel-size").help("Size of the voxel [m].").scan<'f', double>();
-  args.add_argument("sampling-resolution").help("Angular sampling resolution [deg].").scan<'f', double>();
+  args.add_argument("sampling-resolution").help("Angular sampling resolution [deg].").scan<'f',
+    double>();
 
   args.add_argument("--sphere").help("Use spheres instead of voxel").flag();
-  args.add_argument("--scale").default_value(1.0).help("Set to a value between [0,1] to scale markers smaller.").scan<'f', double>();
-  args.add_argument("--save").default_value("").help("Save the reachability map to the provided path.");
-  args.add_argument("--load").default_value("").help("Load the reachability map from the provided path. This will not run the computation again.");
-  args.add_argument("--export-pcd").default_value("").help("Export the map as pointcloud pcd file.");
+  args.add_argument("--scale").default_value(1.0).help(
+    "Set to a value between [0,1] to scale markers smaller.").scan<'f', double>();
+  args.add_argument("--save").default_value("").help(
+    "Save the reachability map to the provided path.");
+  args.add_argument("--load").default_value("").help(
+    "Load the reachability map from the provided path. This will not run the computation again.");
+  args.add_argument("--export-pcd").default_value("").help(
+    "Export the map as pointcloud pcd file.");
 
   try {
     args.parse_args(argc, argv);
-  }
-  catch (const std::exception& err) {
+  } catch (const std::exception & err) {
     std::cerr << err.what() << std::endl;
     std::cerr << args;
     return 1;
@@ -241,9 +266,9 @@ int main(int argc, char *argv[]) {
   string joint_group_name_ = args.get<std::string>("joint-group-name");//"endo";
   double voxel_size = args.get<double>("voxel-size");//0.005;
   double ang_step_size = args.get<double>("sampling-resolution") * (M_PI / 180.0);//M_PI / 360;
-  bool use_sphere = args.get<bool>("--sphere");//true  
+  bool use_sphere = args.get<bool>("--sphere");//true
   double scale = args.get<double>("--scale");
-  if (scale <0.0 || scale >1.0) {
+  if (scale < 0.0 || scale > 1.0) {
     std::cerr << "Scale must be between [0,1]." << std::endl;
     return 1;
   }
@@ -252,18 +277,19 @@ int main(int argc, char *argv[]) {
   std::string export_path = args.get<std::string>("--export-pcd");
   std::cout << load_path << std::endl;
 
-  auto grid_node = make_shared<reachability_map_moveit::ReachabilityMapMoveit>(joint_group_name_, voxel_size, ang_step_size);
-  if(load_path == ""){
+  auto grid_node = make_shared<reachability_map_moveit::ReachabilityMapMoveit>(joint_group_name_,
+    voxel_size, ang_step_size);
+  if(load_path == "") {
     grid_node->generate_reachability_map();
-  }else{
+  } else {
     grid_node->load_reachability_map(load_path);
   }
-  if (save_path != ""){
+  if (save_path != "") {
     grid_node->save_reachability_map(save_path);
   }
-  if (export_path != ""){
-    grid_node -> export_pcd(export_path);
-  } 
+  if (export_path != "") {
+    grid_node->export_pcd(export_path);
+  }
   grid_node->send_marker_message(use_sphere, scale);
 
   grid_node->spin();
